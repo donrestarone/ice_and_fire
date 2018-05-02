b48a1038a6c5a4fa34a9a89335a0aac94211de4e
donrestarone <shashikejayatunge@gmail.com>
2018-05-02 15:21:44 -0400

    finish project

--- test/factories/riders.rb
+++ test/factories/riders.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
   factory :rider do
   	name "Aegon"
+  	nickname "The Conqueror"
+  	email {"#{self.name.downcase}@westros.com" }
+  	#email {"hello@henlo.com"}
   end
 end
+
+# def email_gen(name)
+# 	return "#{name.downcase}@westros.com"
+# end 
